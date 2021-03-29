5f82b393053313aa0c2f48126ef87a5512b558c9
hadarmarom <hadarmarom1@gmail.com>
2021-03-29 18:23:33 +0300

    hadar

--- src/app.vue
+++ src/app.vue
@@ -3,10 +3,17 @@
     <section class="container">
       <div class="nav sub-container">
         <div>
-          <router-link class="main-logo link" to="/"> <img class="logo-image" height="45px" width="65.5" src="  ''" alt=""></router-link>
+          <router-link class="main-logo link" to="/">
+            <img
+              class="logo-image"
+              height="45px"
+              width="65.5"
+              src="http://www.88bit.net/img/Jukify_outline_white.png"
+              alt=""
+          /></router-link>
         </div>
-          <span @click="isMenu = !isMenu" class="menu">☰</span>
-        <div :style="{'max-width': menuDisplay}" class="nav-content">
+        <span @click="isMenu = !isMenu" class="menu">☰</span>
+        <div :style="{ 'max-width': menuDisplay }" class="nav-content">
           <router-link class="link" to="/explore">Explore</router-link>
           <router-link class="link" to="/create">Create station</router-link>
           <router-link class="link" to="/profile">Profile</router-link>
@@ -28,13 +35,13 @@ import playerControl from "./cmps/player-control";
 export default {
   data() {
     return {
-      isMenu: false
-    }
+      isMenu: false,
+    };
   },
   computed: {
     menuDisplay() {
-      return (this.isMenu) ? '100vw' : '0'
-    }
+      return this.isMenu ? "100vw" : "0";
+    },
   },
   async created() {
     await this.$store.dispatch({ type: "loadStations" });
